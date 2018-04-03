83ee8a45668161ce7d1fa27b856ba359f8136938
masein <dan.hosein@yahoo.com>
2018-04-03 00:18:24 -0700

    TODoList app added

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+   
+    @IBOutlet weak var toDoListTable: UITableView!
+    var toDoListArray : [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoListArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "toDoCell")
+        cell.textLabel?.text = toDoListArray[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        toDoListArray = UserDefaults.standard.object(forKey: "toDoListArray") as! [String]
+        toDoListTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete{
+            toDoListArray.remove(at: indexPath.row)
+            tableView.reloadData()
+            UserDefaults.standard.set(toDoListArray, forKey: "toDoListArray")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var label: UITextField!
+    var toDoListArray : [String] = []
+    
+    @IBAction func addToToDOList(_ sender: Any) {
+        if let text = label.text {
+            if let items = UserDefaults.standard.object(forKey: "toDoListArray") as? [String]{
+                toDoListArray = items
+            }
+            toDoListArray.append(text)
+            UserDefaults.standard.set(toDoListArray , forKey: "toDoListArray")
+            label.text = nil
+        }else{
+            print("emptyText ...",label.text as Any)
+        }
+        
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
